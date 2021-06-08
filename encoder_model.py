51afa9bfd373dfde14f9f267f04c2e6c8c72fe61
Ragul Natarajan <44936407+ragul-n@users.noreply.github.com>
2021-06-08 16:46:58 +0530

    corrected indentation in line no. 126

--- encoder_model.py
+++ encoder_model.py
@@ -123,7 +123,7 @@ class MultiHeadAttention(tf.keras.layers.Layer):
   
   
   
-  def point_wise_feed_forward_network(d_model, dff):
+def point_wise_feed_forward_network(d_model, dff):
     return tf.keras.Sequential([
         tf.keras.layers.Dense(dff, activation='relu'),  # (batch_size, seq_len, dff)
         tf.keras.layers.Dense(d_model)  # (batch_size, seq_len, d_model)
